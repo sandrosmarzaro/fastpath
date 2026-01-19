26764701e28c7a44528143c21908e196f385a92d
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-01-19 17:01:14 -0300

    feat: create env variables to cache

--- app/core/settings.py
+++ app/core/settings.py
@@ -11,17 +11,23 @@ class __Settings(BaseSettings):
         extra='ignore',
     )
     DEBUG: bool = True
+    API_URL: str = os.getenv('API_URL', 'http://localhost:8000/')
     DB_URL: str = os.getenv(
         'DB_URL',
         'postgresql+psycopg://user:password@127.0.0.1:5432/database',
     )
     OSRM_URL: str = os.getenv('OSRM_URL', 'http://localhost:5000/')
-    API_URL: str = os.getenv('API_URL', 'http://localhost:8000/')
+    H3_RESOLUTION: int = int(os.getenv('H3_RESOLUTION', '9'))
+
     TOKEN_TYPE: str = os.getenv('TOKEN_TYPE', 'Bearer')
     TOKEN_EXPIRE_MINUTES: int = int(os.getenv('TOKEN_EXPIRE_MINUTES', '60'))
     TOKEN_SECRET_KEY: str = os.getenv('TOKEN_SECRET_KEY', 'secret-key')
     TOKEN_ALGORITHM: str = os.getenv('TOKEN_ALGORITHM', 'HS256')
-    H3_RESOLUTION: int = int(os.getenv('H3_RESOLUTION', '9'))
+
+    CACHE_IP: str = os.getenv('CACHE_IP', 'cache')
+    CACHE_PORT: int = int(os.getenv('CACHE_PORT', '60'))
+    CACHE_PASSWORD = str = os.getenv('CACHE_PASSWORD', 'password')
+    CACHE_TTL_SECONDS: int = int(os.getenv('CACHE_TTL_SECONDS', '600'))
 
 
 @lru_cache
