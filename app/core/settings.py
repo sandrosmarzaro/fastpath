682b4f42c75e16f05185204ab6cb1092550b748f
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-02-06 23:56:30 -0300

    refactor: remove os import in settings and add log level in example

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,39 +1,35 @@
-import os
 from functools import lru_cache
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class __Settings(BaseSettings):
+class _Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=('.env', '.env.prod'),
         env_file_encoding='utf-8',
         extra='ignore',
     )
     DEBUG: bool = True
-    LOG_LEVEL: str = 'DEBUG'
-    API_URL: str = os.getenv('API_URL', 'http://localhost:8000/')
-    DB_URL: str = os.getenv(
-        'DB_URL',
-        'postgresql+psycopg://user:password@127.0.0.1:5432/database',
-    )
-    OSRM_URL: str = os.getenv('OSRM_URL', 'http://localhost:5000/')
-    H3_RESOLUTION: int = int(os.getenv('H3_RESOLUTION', '9'))
+    LOG_LEVEL: str = 'INFO'
+    API_URL: str = 'http://localhost:8000/'
+    DB_URL: str = 'postgresql+psycopg://user:password@127.0.0.1:5432/database'
+    OSRM_URL: str = 'http://localhost:5000/'
+    H3_RESOLUTION: int = 9
 
-    TOKEN_TYPE: str = os.getenv('TOKEN_TYPE', 'Bearer')
-    TOKEN_EXPIRE_MINUTES: int = int(os.getenv('TOKEN_EXPIRE_MINUTES', '60'))
-    TOKEN_SECRET_KEY: str = os.getenv('TOKEN_SECRET_KEY', 'secret-key')
-    TOKEN_ALGORITHM: str = os.getenv('TOKEN_ALGORITHM', 'HS256')
+    TOKEN_TYPE: str = ''
+    TOKEN_EXPIRE_MINUTES: int = 60
+    TOKEN_SECRET_KEY: str = ''
+    TOKEN_ALGORITHM: str = ''
 
-    CACHE_IP: str = os.getenv('CACHE_IP', 'cache')
-    CACHE_PORT: int = int(os.getenv('CACHE_PORT', '60'))
-    CACHE_PASSWORD: str = os.getenv('CACHE_PASSWORD', 'password')
-    CACHE_TTL_SECONDS: int = int(os.getenv('CACHE_TTL_SECONDS', '600'))
+    CACHE_IP: str = 'cache'
+    CACHE_PORT: int = 60
+    CACHE_PASSWORD: str = ''
+    CACHE_TTL_SECONDS: int = 600
 
 
 @lru_cache
-def __get_settings() -> __Settings:
-    return __Settings()
+def _get_settings() -> _Settings:
+    return _Settings()
 
 
-settings = __get_settings()
+settings = _get_settings()
